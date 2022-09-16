616dc73216456b59e0ce44a74b2bb323dc19c97d
fgscarvalho <fgscarvalho97@gmail.com>
2022-09-15 21:22:50 -0300

    New configuration to deploy to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_09_15_214229) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "accounts", force: :cascade do |t|
     t.decimal "balance", precision: 18, scale: 2, default: "0.0", null: false
     t.boolean "is_active", default: true, null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "number_account"
@@ -23,7 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_15_214229) do
 
   create_table "transactions", force: :cascade do |t|
     t.decimal "value", precision: 18, scale: 2, null: false
-    t.integer "account_id", null: false
+    t.bigint "account_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "kind"
